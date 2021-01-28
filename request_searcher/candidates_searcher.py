8a4ea8d4bf59362ae0502ec3dd2632ca05f6ff82
Postnov,Pavel <pavel.postnov@here.com>
2021-01-28 19:17:07 +0300

    minor changes

--- request_searcher/candidates_searcher.py
+++ request_searcher/candidates_searcher.py
@@ -1,1 +1,21 @@
-import request
+import requests
+from serpwow.google_search_results import GoogleSearchResults
+import json
+
+# create the serpwow object, passing in our API key
+serpwow = GoogleSearchResults("9B865C39F952484AAE51D0283C79C735")
+
+# set up a dict for the search parameters
+params = {
+    "q" : "разработка прототипа приложения",
+    "engine" : "yandex"
+}
+
+# retrieve the search results as JSON
+result = serpwow.get_json(params)
+
+# pretty-print the result
+print(json.dumps(result, indent=2, sort_keys=True))
+
+
+# class CandidatesSearcher(object):
