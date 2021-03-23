d75df4ecd39ff52f886ea8582a92ec22fcbc1af0
morozov <lfcbest11@gmail.com>
2021-03-23 21:58:49 +0300

    encoding issue fixed

--- request_searcher/__init__.py
+++ request_searcher/__init__.py
@@ -6,10 +6,11 @@ from tools.var_tools import list_of_dicts_to_csv
 from tools.var_tools import read_text_file
 
 from urllib.parse import urlparse
+import os
 
 
 class RequestSearcher():
-    SEARCHER_API_KEY = ''
+    SEARCHER_API_KEY = "4358ADF7C4004B4DBA7C47988473BEB3"
     IGNORE_HOSTS = r'metadata\ignore_hosts.txt'
 
     def __init__(self):
--- request_searcher/candidates_parser.py
+++ request_searcher/candidates_parser.py
@@ -27,7 +27,7 @@ class WebParser():
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, br'
+            'Accept-Encoding': 'identity'
         }
 
     def clean_url(self, url: str):
